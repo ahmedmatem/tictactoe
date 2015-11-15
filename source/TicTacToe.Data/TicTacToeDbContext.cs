92759a4569de5649db397eafb1e2b3df48dfa283
ahmed <ahmedmatem@gmail.com>
2015-11-15 22:49:42 +0000

    Create Data Project

---
+++ source/TicTacToe.Data/TicTacToeDbContext.cs
@@ -0,0 +1,23 @@
+﻿namespace TicTacToe.Data
+{
+    using System.Data.Entity;
+
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using Models;
+    using Migrations;
+
+    public class TicTacToeDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public TicTacToeDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<TicTacToeDbContext, Configuration>());
+        }
+
+        public static TicTacToeDbContext Create()
+        {
+            return new TicTacToeDbContext();
+        }
+    }
+}
