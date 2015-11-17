146d89deeea5a8f2b2711f964457d557ec486e21
ahmed <ahmedmatem@gmail.com>
2015-11-17 22:17:02 +0000

    Create Game model

---
+++ source/TicTacToe.Models/Game.cs
@@ -0,0 +1,33 @@
+﻿namespace TicTacToe.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class Game
+    {
+        public Game()
+        {
+            this.Id = Guid.NewGuid();
+            this.Board = "---------";
+            this.State = GameState.WaitingForSecondPlayer;
+        }
+
+        public Guid Id { get; set; }
+
+        public GameState State { get; set; }
+
+        [StringLength(9)]
+        [Column(TypeName = "char")]
+        public string Board { get; set; }
+
+        [Required]
+        public string FirstPLayerId { get; set; }
+
+        public string SecondPlayerId { get; set; }
+
+        public virtual ApplicationUser FirstPLayer { get; set; }
+
+        public virtual ApplicationUser SecondPlayer { get; set; }
+    }
+}
---
+++ source/TicTacToe.Models/GameState.cs
@@ -0,0 +1,12 @@
+﻿namespace TicTacToe.Models
+{
+    public enum GameState
+    {
+        WaitingForSecondPlayer = 0,
+        TurnX = 1,
+        TurnO = 2,
+        Draw = 3,
+        FirstPlayerWon = 4,
+        SecondPlayerWon = 5
+    }
+}
