2c7fcb85ad12005443d71febc8ee1eb2ba9223ec
ahmed <ahmedmatem@gmail.com>
2015-11-17 22:16:22 +0000

    Implement Repository and Unit of Work

---
+++ source/TicTacToe.Data/ITicTacToeData.cs
@@ -0,0 +1,14 @@
+﻿namespace TicTacToe.Data
+{
+    using TicTacToe.Data.Repositories;
+    using Models;
+
+    public interface ITicTacToeData
+    {
+        IRepository<ApplicationUser> Users { get; }
+
+        IRepository<Game> Games { get; }
+
+        int SaveChanges();
+    }
+}
